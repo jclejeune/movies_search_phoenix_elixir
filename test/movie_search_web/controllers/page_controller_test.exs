5ce64e71474d5b592c890bb9e3f0ccd8747d3bc1
jchr <jicelejeune@gmail.com>
2025-09-20 14:46:58 +0200

    correction d'un test

--- test/movie_search_web/controllers/page_controller_test.exs
+++ test/movie_search_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MovieSearchWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "🎬 Recherche de Films"
   end
 end
