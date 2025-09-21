83b48f0b2ef7458a6d39df875ab4210bca04f458
jchr <jicelejeune@gmail.com>
2025-09-21 20:32:19 +0200

    ajout de pagination

--- lib/movie_search_web/live/movie_live.ex
+++ lib/movie_search_web/live/movie_live.ex
@@ -2,6 +2,8 @@ defmodule MovieSearchWeb.MovieLive do
   use MovieSearchWeb, :live_view
   alias MovieSearch.Movies
 
+  @items_per_page 3
+
   @impl true
   def mount(_params, _session, socket) do
     movies = Movies.load_movies()
@@ -9,7 +11,8 @@ defmodule MovieSearchWeb.MovieLive do
     socket =
       socket
       |> assign(:all_movies, movies)
-      |> assign(:movies, movies)
+      |> assign(:filtered_movies, movies)  # Films après filtres mais avant pagination
+      |> assign(:movies, [])  # Films de la page courante
       |> assign(:search_query, "")
       |> assign(:selected_genre, "")
       |> assign(:min_rating, 0)
@@ -17,9 +20,15 @@ defmodule MovieSearchWeb.MovieLive do
       |> assign(:genres, Movies.get_all_genres(movies))
       |> assign(:imdb_loading, false)
       |> assign(:imdb_progress, 0)
-      |> assign(:form_mode, :closed)  # :closed, :add, :edit
+      |> assign(:form_mode, :closed)
       |> assign(:editing_movie, nil)
       |> assign(:form_errors, [])
+      # Pagination
+      |> assign(:current_page, 1)
+      |> assign(:items_per_page, @items_per_page)
+      |> assign(:total_pages, 1)
+      |> assign(:total_items, length(movies))
+      |> update_movies()
 
     {:ok, socket}
   end
@@ -29,6 +38,7 @@ defmodule MovieSearchWeb.MovieLive do
     socket =
       socket
       |> assign(:search_query, query)
+      |> assign(:current_page, 1)  # Reset à la page 1
       |> update_movies()
 
     {:noreply, socket}
@@ -38,6 +48,7 @@ defmodule MovieSearchWeb.MovieLive do
     socket =
       socket
       |> assign(:selected_genre, genre)
+      |> assign(:current_page, 1)  # Reset à la page 1
       |> update_movies()
 
     {:noreply, socket}
@@ -53,6 +64,7 @@ defmodule MovieSearchWeb.MovieLive do
     socket =
       socket
       |> assign(:min_rating, min_rating)
+      |> assign(:current_page, 1)  # Reset à la page 1
       |> update_movies()
 
     {:noreply, socket}
@@ -62,6 +74,7 @@ defmodule MovieSearchWeb.MovieLive do
     socket =
       socket
       |> assign(:sort_by, sort_by)
+      |> assign(:current_page, 1)  # Reset à la page 1
       |> update_movies()
 
     {:noreply, socket}
@@ -74,9 +87,55 @@ defmodule MovieSearchWeb.MovieLive do
       |> assign(:selected_genre, "")
       |> assign(:min_rating, 0)
       |> assign(:sort_by, "title")
-      |> assign(:movies, socket.assigns.all_movies)
+      |> assign(:current_page, 1)
+      |> update_movies()
 
     {:noreply, socket}
+  end
+
+  # Gestion de la pagination
+  def handle_event("goto_page", %{"page" => page_str}, socket) do
+    case Integer.parse(page_str) do
+      {page, _} when page >= 1 and page <= socket.assigns.total_pages ->
+        socket =
+          socket
+          |> assign(:current_page, page)
+          |> update_pagination()
+
+        {:noreply, socket}
+      _ ->
+        {:noreply, socket}
+    end
+  end
+
+  def handle_event("prev_page", _, socket) do
+    current_page = socket.assigns.current_page
+    if current_page > 1 do
+      socket =
+        socket
+        |> assign(:current_page, current_page - 1)
+        |> update_pagination()
+
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  def handle_event("next_page", _, socket) do
+    current_page = socket.assigns.current_page
+    total_pages = socket.assigns.total_pages
+
+    if current_page < total_pages do
+      socket =
+        socket
+        |> assign(:current_page, current_page + 1)
+        |> update_pagination()
+
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
   end
 
   def handle_event("check_missing_images", _, socket) do
@@ -90,13 +149,11 @@ defmodule MovieSearchWeb.MovieLive do
         socket.assigns.all_movies
         |> Enum.with_index()
         |> Enum.map(fn {movie, index} ->
-          # Progress
           progress = round((index + 1) / total * 100)
           send(pid, {:imdb_progress, progress})
 
-          # Vérifie et répare si nécessaire
           if needs_repair?(movie) do
-            Process.sleep(800)  # Rate limiting
+            Process.sleep(800)
             Movies.enrich_with_imdb(movie)
           else
             movie
@@ -140,7 +197,6 @@ defmodule MovieSearchWeb.MovieLive do
   def handle_event("add_movie", %{"movie" => movie_params}, socket) do
     case validate_movie_params(movie_params) do
       [] ->
-        # Validation OK
         case Movies.add_movie(socket.assigns.all_movies, movie_params) do
           {:ok, updated_movies, _new_movie} ->
             socket =
@@ -158,7 +214,6 @@ defmodule MovieSearchWeb.MovieLive do
         end
 
       errors ->
-        # Validation failed
         {:noreply, assign(socket, :form_errors, errors)}
     end
   end
@@ -204,14 +259,25 @@ defmodule MovieSearchWeb.MovieLive do
     updated_movies = Enum.reject(socket.assigns.all_movies, &(&1.id == id))
     Movies.save_movies(updated_movies)
 
+    # Si on supprime le dernier film de la page et qu'on n'est pas sur la page 1
+    current_page = socket.assigns.current_page
+    filtered_count = length(apply_filters(updated_movies, socket.assigns))
+    total_pages = calculate_total_pages(filtered_count, @items_per_page)
+
+    new_current_page = if current_page > total_pages and total_pages > 0 do
+      total_pages
+    else
+      current_page
+    end
+
     {:noreply,
      socket
      |> assign(:all_movies, updated_movies)
      |> assign(:genres, Movies.get_all_genres(updated_movies))
+     |> assign(:current_page, new_current_page)
      |> update_movies()}
   end
 
-  # Fonction helper pour détecter si une réparation est nécessaire
   defp needs_repair?(%Movies{poster: poster}) do
     cond do
       String.starts_with?(poster, "/images/posters/") ->
@@ -229,7 +295,6 @@ defmodule MovieSearchWeb.MovieLive do
   end
 
   def handle_info({:imdb_complete, enriched_movies}, socket) do
-    # Sauvegarde les films avec les nouvelles images
     Movies.save_movies(enriched_movies)
 
     socket =
@@ -242,34 +307,70 @@ defmodule MovieSearchWeb.MovieLive do
     {:noreply, socket}
   end
 
-  # Fonction privée pour mettre à jour la liste des films affichés
-  defp update_movies(socket) do
+  # Fonction pour appliquer les filtres sans pagination
+  defp apply_filters(movies, assigns) do
     %{
-      all_movies: all_movies,
       search_query: search_query,
       selected_genre: selected_genre,
       min_rating: min_rating,
       sort_by: sort_by
-    } = socket.assigns
+    } = assigns
 
-    movies =
-      all_movies
-      |> Movies.search_movies(search_query)
-      |> Movies.filter_by_genre(selected_genre)
-      |> Movies.filter_by_rating(min_rating)
-      |> Movies.sort_movies(sort_by)
+    movies
+    |> Movies.search_movies(search_query)
+    |> Movies.filter_by_genre(selected_genre)
+    |> Movies.filter_by_rating(min_rating)
+    |> Movies.sort_movies(sort_by)
+  end
 
+  # Fonction pour calculer la pagination
+  defp paginate(movies, current_page, items_per_page) do
+    start_index = (current_page - 1) * items_per_page
+    Enum.slice(movies, start_index, items_per_page)
+  end
+
+  defp calculate_total_pages(total_items, items_per_page) when total_items == 0, do: 1
+  defp calculate_total_pages(total_items, items_per_page) do
+    ceil(total_items / items_per_page)
+  end
+
+  # Fonction privée pour mettre à jour les films avec pagination complète
+  defp update_movies(socket) do
+    filtered_movies = apply_filters(socket.assigns.all_movies, socket.assigns)
+    total_items = length(filtered_movies)
+    total_pages = calculate_total_pages(total_items, @items_per_page)
+
+    # Ajuster la page courante si nécessaire
+    current_page = socket.assigns.current_page
+    adjusted_current_page = if current_page > total_pages and total_pages > 0 do
+      total_pages
+    else
+      current_page
+    end
+
+    movies = paginate(filtered_movies, adjusted_current_page, @items_per_page)
+
+    socket
+    |> assign(:filtered_movies, filtered_movies)
+    |> assign(:movies, movies)
+    |> assign(:total_items, total_items)
+    |> assign(:total_pages, total_pages)
+    |> assign(:current_page, adjusted_current_page)
+  end
+
+  # Fonction pour mettre à jour seulement la pagination (sans recalculer les filtres)
+  defp update_pagination(socket) do
+    movies = paginate(socket.assigns.filtered_movies, socket.assigns.current_page, @items_per_page)
     assign(socket, :movies, movies)
   end
 
-  # Fonction utilitaire pour formater la durée
+  # Fonctions utilitaires
   def format_duration(minutes) do
     hours = div(minutes, 60)
     mins = rem(minutes, 60)
     "#{hours}h #{mins}min"
   end
 
-  # Fonction utilitaire pour formater les genres
   def format_genres(genres) do
     Enum.join(genres, ", ")
   end
@@ -280,6 +381,24 @@ defmodule MovieSearchWeb.MovieLive do
   def form_is_edit?(form_mode), do: form_mode == :edit
   def has_errors?(errors) when is_list(errors), do: length(errors) > 0
   def has_errors?(_), do: false
+
+  # Helpers pour la pagination
+  def has_prev_page?(current_page), do: current_page > 1
+  def has_next_page?(current_page, total_pages), do: current_page < total_pages
+
+  def page_range(current_page, total_pages) do
+    start_page = max(1, current_page - 2)
+    end_page = min(total_pages, current_page + 2)
+    start_page..end_page
+  end
+
+  def start_item_number(current_page, items_per_page) do
+    (current_page - 1) * items_per_page + 1
+  end
+
+  def end_item_number(current_page, items_per_page, total_items) do
+    min(current_page * items_per_page, total_items)
+  end
 
   # Validation helper
   defp validate_movie_params(params) do
